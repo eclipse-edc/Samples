5293459a58f85a2dd2aab0a96cfae2cba93c01a4
farhin23 <farhin.jahan@isst.fraunhofer.de>
2023-03-21 14:27:27 +0100

    feat: create consumer and provider connector for provision policy sample

---
+++ policy/policy-02-provision/policy-provision-provider/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    `java-library`
+    id("application")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+val groupId: String by project
+val edcVersion: String by project
+
+dependencies {
+    implementation("$groupId:control-plane-core:$edcVersion")
+
+    implementation("$groupId:api-observability:$edcVersion")
+
+    implementation("$groupId:configuration-filesystem:$edcVersion")
+    implementation("$groupId:iam-mock:$edcVersion")
+
+    implementation("$groupId:auth-tokenbased:$edcVersion")
+    implementation("$groupId:management-api:$edcVersion")
+
+    implementation("$groupId:ids:$edcVersion")
+
+    implementation(project(":policy:policy-02-provision:policy-provision-provider-policy-functions"))
+    implementation(project(":policy:policy-02-provision:policy-transfer-file-local"))
+}
+
+application {
+    mainClass.set("org.eclipse.edc.boot.system.runtime.BaseRuntime")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xm")
+    mergeServiceFiles()
+    archiveFileName.set("provider.jar")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -88,6 +88,8 @@ include(":policy:policy-02-provision:policy-provision")
 include(":policy:policy-02-provision:policy-transfer-file-local")
 include(":policy:policy-02-provision:policy-provision-provider-policy-functions")
 include(":policy:policy-02-provision:policy-provision-consumer-policy-functions")
+include(":policy:policy-02-provision:policy-provision-provider")
+include(":policy:policy-02-provision:policy-provision-consumer")
 
 // modules for code samples ------------------------------------------------------------------------
 include(":other:custom-runtime")
