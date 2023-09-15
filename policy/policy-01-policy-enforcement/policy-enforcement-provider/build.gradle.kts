eb67142f19bc8cc9c0d0a5abcaeda770fc706a4e
Mohammad Majadly <mohammad.majadly@isst.fraunhofer.de>
2023-09-15 16:20:43 +0200

    chore: update build and settings of gradle

--- policy/policy-01-policy-enforcement/policy-enforcement-provider/build.gradle.kts
+++ policy/policy-01-policy-enforcement/policy-enforcement-provider/build.gradle.kts
@@ -1,19 +1,17 @@
 plugins {
     `java-library`
     id("application")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    alias(libs.plugins.shadow)
 }
 
-val groupId: String by project
-val edcVersion: String by project
 
 dependencies {
-    implementation("$groupId:control-plane-core:$edcVersion")
+    implementation(libs.edc.control.plane.core)
 
-    implementation("$groupId:configuration-filesystem:$edcVersion")
-    implementation("$groupId:iam-mock:$edcVersion")
+    implementation(libs.edc.configuration.filesystem)
+    implementation(libs.edc.iam.mock)
 
-    implementation("$groupId:ids:$edcVersion")
+    implementation(libs.edc.dsp)
 
     implementation(project(":policy:policy-01-policy-enforcement:policy-functions"))
 }
