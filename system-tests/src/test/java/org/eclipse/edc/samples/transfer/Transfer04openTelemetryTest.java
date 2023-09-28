de3f57be79d41c4c42c2f62a5de796e1ccef6152
hamidonos <hamid@idonos-solutions.com>
2023-09-28 14:48:49 +0200

    test: download OpenTelemetry Jar before test execution

--- system-tests/src/test/java/org/eclipse/edc/samples/transfer/Transfer04openTelemetryTest.java
+++ system-tests/src/test/java/org/eclipse/edc/samples/transfer/Transfer04openTelemetryTest.java
@@ -15,6 +15,7 @@
 package org.eclipse.edc.samples.transfer;
 
 import org.eclipse.edc.junit.annotations.EndToEndTest;
+import org.eclipse.edc.spi.EdcException;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,19 +24,23 @@ import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
 
 @EndToEndTest
 @Testcontainers
 public class Transfer04openTelemetryTest {
 
+    private static final String OPEN_TELEMETRY_JAR_URL = "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.27.0/opentelemetry-javaagent.jar";
     private static final String SAMPLE_FOLDER = "transfer/transfer-04-open-telemetry";
-    static final String CONTRACT_OFFER_FILE_PATH = SAMPLE_FOLDER + "/contractoffer.json";
-
-    static final String TRANSFER_FILE_PATH = SAMPLE_FOLDER + "/filetransfer.json";
-
-    static final String SAMPLE_ASSET_FILE_PATH = SAMPLE_FOLDER + "/README.md";
-    static final String DESTINATION_FILE_PATH = "transfer/requested.test.txt";
+    private static final String CONTRACT_OFFER_FILE_PATH = SAMPLE_FOLDER + "/contractoffer.json";
+    private static final String OPEN_TELEMETRY_JAR_PATH = SAMPLE_FOLDER + "/opentelemetry-javaagent.jar";
+    private static final String TRANSFER_FILE_PATH = SAMPLE_FOLDER + "/filetransfer.json";
+    private static final String SAMPLE_ASSET_FILE_PATH = SAMPLE_FOLDER + "/README.md";
+    private static final String DESTINATION_FILE_PATH = "transfer/requested.test.txt";
     final FileTransferSampleTestCommon testUtils = new FileTransferSampleTestCommon(SAMPLE_ASSET_FILE_PATH, DESTINATION_FILE_PATH);
 
     @ClassRule
@@ -45,6 +50,7 @@ public class Transfer04openTelemetryTest {
 
     @BeforeAll
     static void setUp() {
+        downloadOpenTelemetryJar();
         environment.start();
     }
 
@@ -62,5 +68,18 @@ public class Transfer04openTelemetryTest {
     @AfterEach
     protected void tearDown() {
         testUtils.cleanTemporaryTestFiles();
+    }
+
+    private static void downloadOpenTelemetryJar() {
+        try (var in = new BufferedInputStream(new URL(OPEN_TELEMETRY_JAR_URL).openStream());
+             var out = new FileOutputStream("../" + OPEN_TELEMETRY_JAR_PATH)) {
+            var dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                out.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            throw new EdcException(e);
+        }
     }
 }
