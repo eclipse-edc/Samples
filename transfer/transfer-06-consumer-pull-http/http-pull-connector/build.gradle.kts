eb24bd1bf017fc5710e2647e8e416b3e8cfe3758
Aniket Talele <talele.aniket@gmail.com>
2023-10-11 10:49:24 +0530

    add missing edc-control-plane-api-client dependency

--- transfer/transfer-06-consumer-pull-http/http-pull-connector/build.gradle.kts
+++ transfer/transfer-06-consumer-pull-http/http-pull-connector/build.gradle.kts
@@ -19,6 +19,7 @@ plugins {
 }
 
 dependencies {
+    implementation(libs.edc.control.plane.api.client)
     implementation(libs.edc.control.plane.core)
     implementation(libs.edc.dsp)
     implementation(libs.edc.configuration.filesystem)
