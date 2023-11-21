7b498da86eaa70c7c277555cc17abde3a90b0ceb
majadlymhmd <mohammad.majadly@isst.fraunhofer.de>
2023-11-21 17:26:09 +0100

    update: Policy Sample update

--- system-tests/src/test/java/org/eclipse/edc/samples/policy/Policy01BasicTest.java
+++ system-tests/src/test/java/org/eclipse/edc/samples/policy/Policy01BasicTest.java
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2023 Fraunhofer Institute for Software and Systems Engineering
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Fraunhofer Institute for Software and Systems Engineering - initial API and implementation
+ *
+ */
+
 package org.eclipse.edc.samples.policy;
 
 import org.eclipse.edc.junit.annotations.EndToEndTest;
@@ -12,12 +26,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @EndToEndTest
 class Policy01BasicTest {
+
     @RegisterExtension
     static EdcRuntimeExtension consumer = new EdcRuntimeExtension(
             ":policy:policy-01-policy-enforcement:policy-enforcement-consumer",
             "consumer",
             emptyMap()
     );
+
     @Test
     void shouldStartConsumer() {
         assertThat(consumer.getContext().getService(Clock.class)).isNotNull();
