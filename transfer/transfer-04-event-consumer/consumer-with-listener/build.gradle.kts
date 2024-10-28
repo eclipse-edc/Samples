ecdfe86436897c4dc0e6f9c70cbbd3dcbed2841f
ndr_brt <andrea.bertagnolli@gmail.com>
2024-10-28 16:17:05 +0100

    upgrade wrapper

--- transfer/transfer-04-event-consumer/consumer-with-listener/build.gradle.kts
+++ transfer/transfer-04-event-consumer/consumer-with-listener/build.gradle.kts
@@ -19,7 +19,34 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":transfer:transfer-00-prerequisites:connector"))
+    implementation(libs.edc.control.api.configuration)
+    implementation(libs.edc.control.plane.api.client)
+    implementation(libs.edc.control.plane.api)
+    implementation(libs.edc.control.plane.core)
+    implementation(libs.edc.token.core)
+    implementation(libs.edc.dsp)
+    implementation(libs.edc.http)
+    implementation(libs.edc.configuration.filesystem)
+    implementation(libs.edc.iam.mock)
+    implementation(libs.edc.management.api)
+    implementation(libs.edc.transfer.data.plane.signaling)
+    implementation(libs.edc.transfer.pull.http.receiver)
+    implementation(libs.edc.validator.data.address.http.data)
+
+    implementation(libs.edc.edr.cache.api)
+    implementation(libs.edc.edr.store.core)
+    implementation(libs.edc.edr.store.receiver)
+
+    implementation(libs.edc.data.plane.selector.api)
+    implementation(libs.edc.data.plane.selector.core)
+
+    implementation(libs.edc.data.plane.self.registration)
+    implementation(libs.edc.data.plane.signaling.api)
+    implementation(libs.edc.data.plane.public.api)
+    implementation(libs.edc.data.plane.core)
+    implementation(libs.edc.data.plane.http)
+    implementation(libs.edc.data.plane.iam)
+
     implementation(project(":transfer:transfer-04-event-consumer:listener"))
 }
 
